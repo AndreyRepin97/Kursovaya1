e20ffd5ffa9366c17d8c982f517d09090f2f275a
porwa <o-la-la-repka@yandex.ru>
2022-05-11 22:32:05 +0300

    Finished_kurs

---
+++ src/ru/skypro/Emploee.java
@@ -0,0 +1,48 @@
+package ru.skypro;
+
+public class Emploee{
+
+    private String FIO;
+    private int department;
+    private double salary;
+    public static int idEm;
+    public static int id;
+
+    public Emploee (String FIO, int department, double salary){
+
+        this.FIO=FIO;
+        this.department=department;
+        this.salary=salary;
+        id++;
+        idEm=id;
+
+    }
+
+    public String toString(){
+        return FIO + "  " + department + "  " + salary + "  " +  idEm;
+    }
+
+    public String getFIO(){
+        return FIO;
+    }
+
+    public int getDepartment(){
+        return department;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public int getIdEm(){
+        return idEm;
+    }
+
+    public void setSalary(double salary){
+        this.salary=salary;
+    }
+
+    public void setDepartment(int department){
+        this.department=department;
+    }
+}
---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,81 @@
+package ru.skypro;
+
+public class Main {
+
+    public static double sumSal(Emploee[] emploees) {
+        double sum = 0;
+        for (int i = 1; i < emploees.length; i++) {
+            sum = sum + emploees[i].getSalary();
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static double minSal(Emploee[] emploees) {
+        double min = 999999;
+        for (int i = 0; i < emploees.length; i++) {
+            if (emploees[i] == null) {
+                break;
+                } else {
+                if (emploees[i].getSalary() < min) {
+                    min = emploees[i].getSalary();
+                }
+            }
+        }
+        System.out.println(min);
+        return min;
+    }
+
+    public static double maxSal(Emploee[] emploees){
+        double max=0;
+        for(int i = 0; i < emploees.length; i++){
+            if(emploees[i]==null) {
+                break;
+            }else{
+                if(emploees[i].getSalary() > max)
+                max = emploees[i].getSalary();
+            }
+        }
+        System.out.println(max);
+        return max;
+    }
+
+    public static double sredSal(Emploee[] emploees){
+        double sred=0;
+        double count=1;
+        for(int i = 1; i < emploees.length; i++){
+            if(emploees[i]!=null) {
+                count++;
+            }
+                sred=sred+emploees[i].getSalary();
+            }
+        sred=sred/count;
+        System.out.println(sred);
+        return sred;
+    }
+
+    public static void main(String[] args) {
+        Emploee[] emploees = new Emploee[5];
+
+
+        emploees[0]=new Emploee("Ivanov Ivan Ivanovich", 1, 15000);
+        emploees[1]=new Emploee("Vladimirov Vladimir Vladimirovich", 2, 17000);
+        emploees[2]=new Emploee("Alexandrov Alexandr Alexandrovich", 3, 18500);
+        emploees[3]=new Emploee("Petrov Petr Petrovich", 1, 13750);
+        emploees[4]=new Emploee("Andreev Andrey Andreevich", 2, 14250);
+        System.out.println(emploees[0].toString());
+        System.out.println(emploees[1].toString());
+        System.out.println(emploees[2].toString());
+        System.out.println(emploees[3].toString());
+        System.out.println(emploees[4].toString());
+
+        sumSal(emploees);
+        minSal(emploees);
+        maxSal(emploees);
+        sredSal(emploees);
+
+    }
+}
+
+
+
